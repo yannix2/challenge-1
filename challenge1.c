3236250b3b3fb47d5038ae82269986963b231921
yassine khiari <65060923+yannix2@users.noreply.github.com>
2021-06-27 02:14:17 +0100

    Add files via upload

---
+++ challenge1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float a,b;
+    printf("donner l'hauteur' \n");
+    scanf ("%f",&a);
+
+    printf("donner la base \n");
+    scanf ("%f",&b);
+    float c=(a*b)/2;
+
+    printf ("l'aire du triangle est %f\n",c);
+
+
+
+    return 0;
+}
